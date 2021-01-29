e9caca6e6498e352c652e6fe6eaba37b52ee7e59
jaboro <jaboro@oakland.edu>
2021-01-28 23:42:36 -0500

    Finished test commit

--- core/src/sophomoreproject/game/screens/LoginScreen.java
+++ core/src/sophomoreproject/game/screens/LoginScreen.java
@@ -1,8 +1,11 @@
 package sophomoreproject.game.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 
-public class LoginScreen implements Screen {
+public class  LoginScreen implements Screen, Input.TextInputListener {
+
     @Override
     public void show() {
 
@@ -37,4 +40,16 @@ public class LoginScreen implements Screen {
     public void dispose() {
 
     }
+    LoginScreen listener = new LoginScreen();
+    @Override
+    public void input(String text) {
+        Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");
+
+    }
+
+    @Override
+    public void canceled() {
+
+    }
+
 }
