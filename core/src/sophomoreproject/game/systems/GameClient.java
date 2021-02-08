9f218602899709f714f0a69ea662e0c4bbd2525f
Jonah Shader <jonah.shader@gmail.com>
2021-02-07 20:00:58 -0500

    worked on server client stuff. also GameWorld

--- core/src/sophomoreproject/game/systems/GameClient.java
+++ core/src/sophomoreproject/game/systems/GameClient.java
@@ -1,9 +1,28 @@
 package sophomoreproject.game.systems;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import sophomoreproject.game.networking.ClientNetwork;
+
 public class GameClient {
+    private final ClientNetwork client = ClientNetwork.getInstance();
     private GameWorld world;
+
+    public GameClient() {
+        client.addListener(new Listener(){
+            @Override
+            public void received(Connection c, Object o) {
+//                if (o instanceof )
+            }
+        });
+    }
 
     public void run(float dt) {
         world.update(dt);
+    }
+
+    public void draw(SpriteBatch sb) {
+        world.draw(sb);
     }
 }
