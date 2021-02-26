e5c508888448992c551e2090684b6793eb7c8bc3
Renyah-A <abdulmaseeh2@oakland.edu>
2021-02-26 16:47:28 -0500

    add documentation to the sound system, explain class play sound in the world

--- core/src/sophomoreproject/game/singletons/SoundSystem.java
+++ core/src/sophomoreproject/game/singletons/SoundSystem.java
@@ -26,12 +26,22 @@ public final class SoundSystem {
         return instance;
     }
 
+    /**
+     * play sound with calculated panning related to sound and camera position
+     * if sound position is left of camera position the panning is negative
+     * if sound position is right of camera position the panning is positive
+     * @param s the sound play
+     * @param soundPosition  the position of the sound in game world
+     * @param cameraPosition the position of the camera in game world
+     * @param volume the range (0,1) to play the sound in the game
+     */
     public void playSoundInWorld(Sound s, Vector2 soundPosition, Vector2 cameraPosition, float volume) {
 
     }
 
     public void playSoundStandalone(Sound s, float volume, float pan) {
         float realVolume = volume * overallVolume;
+        s.play(realVolume, 1, pan);
     }
 
     public void setOverallVolume(float overallVolume) {
