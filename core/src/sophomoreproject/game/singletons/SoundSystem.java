78d5cf90d2e7215739829fbe5a958b4a35a9bd2d
Renyah-A <abdulmaseeh2@oakland.edu>
2021-03-04 22:23:45 -0500

    added play sound group method, sound system array list

--- core/src/sophomoreproject/game/singletons/SoundSystem.java
+++ core/src/sophomoreproject/game/singletons/SoundSystem.java
@@ -2,20 +2,29 @@ package sophomoreproject.game.singletons;
 
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
 
 public final class SoundSystem {
     private static SoundSystem instance;
     private CustomAssetManager assets;
 
     private float overallVolume = 1f;
+    private ArrayList<Sound> bulletSounds, footstepSounds;
 
-//    enum SoundGroup {
-//        BULLET,
-//        FOOT_STEP
-//    }
+    enum SoundGroup {
+        BULLET,
+        FOOT_STEP
+    }
 
     private SoundSystem() {
         assets = CustomAssetManager.getInstance();
+        bulletSounds = new ArrayList<>();
+        footstepSounds = new ArrayList<>();
+
+        //  bulletSound.add(assets.manager.get());
+        // footstepSound.add(assets.manager.get());
     }
 
     public static SoundSystem getInstance() {
@@ -30,15 +39,16 @@ public final class SoundSystem {
      * play sound with calculated panning related to sound and camera position
      * if sound position is left of camera position the panning is negative
      * if sound position is right of camera position the panning is positive
-     * @param s the sound play
+     *
+     * @param s              the sound play
      * @param soundPosition  the position of the sound in game world
      * @param cameraPosition the position of the camera in game world
-     * @param volume the range (0,1) to play the sound in the game
+     * @param volume         the range (0,1) to play the sound in the game
      */
     public void playSoundInWorld(Sound s, Vector2 soundPosition, Vector2 cameraPosition, float volume) {
         float prePan = soundPosition.x - cameraPosition.x;
         float realVolume = volume * overallVolume;
-        s.play(realVolume, 1, prePan/320);
+        s.play(realVolume, 1, prePan / 320);
     }
 
     public void playSoundStandalone(Sound s, float volume, float pan) {
@@ -50,7 +60,20 @@ public final class SoundSystem {
         this.overallVolume = overallVolume;
 
     }
-    public void playGroupSound(Sound s, Vector2 soundPosition, Vector2 cameraPosition, float volume){
 
+    public void playSoundGroup(SoundGroup s, Vector2 soundPosition, Vector2 cameraPosition, float volume) {
+        switch (s) {
+
+            case BULLET:
+
+                playSoundInWorld(bulletSounds.get((int) (Math.random() * bulletSounds.size())), soundPosition, cameraPosition, volume);
+                break;
+            case FOOT_STEP:
+
+                playSoundInWorld(footstepSounds.get((int) (Math.random() * footstepSounds.size())), soundPosition, cameraPosition, volume);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + s);
+        }
     }
 }
