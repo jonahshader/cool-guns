7596e9dd46f26a434c734d49b07f291ad213ab0d
B453L <baselhanna00@gmail.com>
2021-03-20 22:43:21 -0400

    Options->Screen->FrameRate is freezing. Options->Controls->Preferences is also freezing.

---
+++ core/src/sophomoreproject/game/screens/PreferencesScreen.java
@@ -0,0 +1,50 @@
+package sophomoreproject.game.screens;
+
+import com.badlogic.gdx.Screen;
+import sophomoreproject.game.CoolGuns;
+
+public class PreferencesScreen implements Screen {
+
+    private CoolGuns game;
+    private int accountID;
+
+    public PreferencesScreen(CoolGuns game, int accountID) {
+        this.game = game;
+        this.accountID = accountID;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
